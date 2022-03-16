047fc7e8eccbf08331b978523c79acd8ab75e333
Anupama Ravindran <anupamaravindran123@gmail.com>
2022-03-16 19:35:41 +0530

    finished

--- form.js
+++ form.js
@@ -1,12 +1,53 @@
-<script>
-    function validateform() {
-        var name=dociment.myform.name.value;
-        var password=document.myform.password.value;
-        if(name==null || name==""){
-            alert("Please fill the box")
-        }else if(password.lenght<5){
-            alert("Password must be 8 charactores")
-        }
-        
-    }
-</script>
+// const form = document.querySelector("form[name='contact-form']");
+// const nameInput = document.querySelector("input[name='name']");
+// const emailInput = document.querySelector("input[name='email']");
+// const phoneInput = document.querySelector("input[name='phone']");
+// const messageInput = document.querySelector("textarea[name='message']");
+
+// nameInput.isValid = () => !!nameInput.value;
+// emailInput.isValid = () => isValidEmail(emailInput.value);
+// phoneInput.isValid = () => isValidPhone(phoneInput.value);
+// messageInput.isValid = () => !!messageInput.value;
+
+// const inputFields = [nameInput, emailInput, phoneInput, messageInput];
+
+// const isValidEmail = (email) => {
+//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// };
+
+// const isValidPhone = (phone) => {
+//   const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+//   return re.test(String(phone).toLowerCase());
+// };
+
+// let shouldValidate = false;
+// let isFormValid = false;
+
+// const validateInputs = () => {
+//   console.log("we are here");
+//   if (!shouldValidate) return;
+
+//   isFormValid = true;
+//   inputFields.forEach((input) => {
+//     input.classList.remove("invalid");
+//     input.nextElementSibling.classList.add("fade");
+
+//     if (!input.isValid()) {
+//       input.classList.add("invalid");
+//       isFormValid = false;
+//       input.nextElementSibling.classList.remove("fade");
+//     }
+//   });
+// };
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   shouldValidate = true;
+//   validateInputs();
+//   if (isFormValid) {
+//     // TODO: DO AJAX REQUEST
+//   }
+// });
+
+// inputFields.forEach((input) => input.addEventListener("input", validateInputs));
